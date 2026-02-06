32fab7e7a630bf8f36149a3cc0703c95c0bcf038
ZooIander <97798142+ZooIander@users.noreply.github.com>
2026-02-06 06:10:17 +0300

    Update server.js
    Fix dir

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import express from "express";
-import handler from "speak.js";
+import handler from "./api/speak.js";
 
 const app = express();
 
-app.use(express.json({ limit: "1mb" }));
 
-app.post("", (req, res) => handler(req, res));
+app.use(express.json({ limit: "5mb" }));
+
+app.post("/api/speak", (req, res) => handler(req, res));
 
 // healthcheck
-app.get("", (req, res) => res.send("OK"));
+app.get("/", (req, res) => res.send("OK"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Listening on", port));
